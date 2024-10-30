1b898ef4e8af02ec726b1c63bbbb322748219bca
mechanicmuthu <30587093+mechanicmuthu@users.noreply.github.com>
2024-10-30 22:17:31 +0530

    Update codedump.py

--- codedump/codedump.py
+++ codedump/codedump.py
@@ -166,6 +166,7 @@ def should_skip(path):
         r'^log\.',  # Matches log.txt, log.old, etc.
         r'\.bak$',
         r'\_bak.$',
+        r'_bak\..*$',
         r'\.pickle$',
         r'\.tmp$',
         r'\.temp$',
